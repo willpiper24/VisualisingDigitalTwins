5e3e87162e914e8e5781aecd653252b59334bdc9
willpiper <willpiper24@gmail.com>
2022-02-03 14:12:11 +0000

    update

--- LSTM_RMSE.py
+++ LSTM_RMSE.py
@@ -3,8 +3,8 @@ import math
 import csv
 import matplotlib.pyplot as plt
 
-def open_csv(steps_in,type):
-    with open(steps_in + "_steps_in_" + type + ".csv", newline='') as csv_file:
+def open_csv(steps_in,sample_size,type):
+    with open(f"{steps_in}_steps_in_{sample_size}_sample_{type}.csv", newline='') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter = ' ')
         data = []
         for row in csv_reader:
@@ -15,19 +15,17 @@ def open_csv(steps_in,type):
 
 steps_in_plot = []
 RMSE_plot = []
-for steps_in in range(2,48,2):
-    real_data = open_csv(str(steps_in),"real")
-    predicted_data = open_csv(str(steps_in),"predicted")
-    if not np.any(np.isnan(predicted_data)):
+for steps_in in range(2,25):
+    RMSE_array = []
+    for sample_size in range(96*7,96*7*52,96*7*13):
+        real_data = open_csv(str(steps_in),sample_size,"real")
+        predicted_data = open_csv(str(steps_in),sample_size,"predicted")
         MSE = np.square(np.subtract(real_data,predicted_data)).mean()
         RMSE = math.sqrt(MSE)
-        RMSE_plot.append(RMSE)
+        RMSE_array.append(RMSE)
+    if not np.any(np.isnan(RMSE_array)):
         steps_in_plot.append(steps_in)
+        RMSE_plot.append(np.average(RMSE_array))
 
 print(steps_in_plot)
 print(RMSE_plot)
-plt.scatter(steps_in_plot,RMSE_plot)
-plt.xticks(range(min(steps_in_plot), max(steps_in_plot)+1, 2))
-plt.xlabel("Number of Steps In")
-plt.ylabel("RMSE (Root Mean Squared Error)")
-plt.savefig("LSTM_RMSE.png")
